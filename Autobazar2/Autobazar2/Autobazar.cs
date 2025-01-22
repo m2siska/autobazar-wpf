5f7a464662d9f06928091efaa1e079da12e8fbf1
MaxSiska <maxsiska81@gmail.com>
2025-01-22 18:06:55 +0100

    Add files via upload

---
+++ Autobazar2/Autobazar2/Autobazar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Xml.Serialization;
+
+namespace Autobazar2
+{
+    [XmlRoot(ElementName = "Auto")]
+    public class Auto
+    {
+        [XmlElement(ElementName = "Model")]
+        public string Model { get; set; }
+
+        [XmlElement(ElementName = "Datum")]
+        public string Datum { get; set; }
+
+        [XmlIgnore]
+        public DateTime DatumStruct
+        {
+            get
+            {
+                string[] formats = { "d.M.yyyy", "dd.MM.yyyy", "d.MM.yyyy", "dd.M.yyyy" };
+                DateTime.TryParseExact(Datum, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime datum);
+                    return datum;
+
+            }
+            set
+            {
+                Datum = value.ToString("dd.MM.yyyy");
+            }
+        }
+
+        [XmlElement(ElementName = "Cena")]
+        public double Cena { get; set; }
+
+        [XmlElement(ElementName = "DPH")]
+        public double DPH { get; set; }
+    }
+
+    [XmlRoot(ElementName = "autobazar")]
+    public class Autobazar
+    {
+        [XmlElement(ElementName = "Auto")]
+        public List<Auto> Auto { get; set; }
+    }
+
+    public class Vikend
+    {
+        public string Model { get; set; }
+        public double Cena { get; set; }
+        public double DPH { get; set; }
+        public double CenaDPH { get; set; }
+
+        public Vikend(string model, double cena, double cenaDPH)
+        {
+            Model = model;
+            Cena = cena;
+            CenaDPH = cenaDPH;
+        }
+
+    }
+}  
---
+++ Autobazar2/Autobazar2/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Win32;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Autobazar2
+{
+    public partial class MainWindow : Window
+    {
+        Autobazar autobazar;
+        List<Vikend> vikend = new List<Vikend>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FSButton(object sender, RoutedEventArgs e)
+        {
+            if (autobazar != null && autobazar.Auto != null && autobazar.Auto.Count > 0)
+            {
+                autobazar.Auto.Clear();
+            }
+            if (vikend != null && vikend.Count > 0)
+            {
+                vikend.Clear();
+            }
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "XML File | *.xml";
+            fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+            bool? success = fileDialog.ShowDialog();
+            string path = fileDialog.FileName;
+            bool serialization = false;
+
+            if (success == true)
+            {
+                path = fileDialog.FileName;
+                DeserializeXML(path);
+                serialization = true;
+            }
+
+
+            if (serialization == true)
+            {
+
+            prodejeaut.ItemsSource = autobazar.Auto;
+            prodejeaut.Columns[2].Visibility = Visibility.Hidden;
+            DataGridTextColumn cenaImport = prodejeaut.Columns[3] as DataGridTextColumn;
+                if (cenaImport != null)
+                {
+                    Binding newBinding = new Binding
+                    {
+                        Path = ((Binding)cenaImport.Binding).Path,
+                        StringFormat = "N",
+                        ConverterCulture = CultureInfo.CreateSpecificCulture("cs-CZ")
+                    };
+                    cenaImport.Binding = newBinding;
+                }
+
+                CenaDPH(vikend);
+            prodejVikend.ItemsSource = vikend;
+            DataGridTextColumn vikendDPH = prodejVikend.Columns[1] as DataGridTextColumn;
+                if (vikendDPH != null)
+                {
+                    Binding newBinding = new Binding
+                    {
+                        Path = ((Binding)vikendDPH.Binding).Path,
+                        StringFormat = "N",
+                        ConverterCulture = CultureInfo.CreateSpecificCulture("cs-CZ")
+                    };
+                    vikendDPH.Binding = newBinding;
+                }
+            }
+
+        }
+        public void DeserializeXML(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Autobazar));
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                autobazar = (Autobazar)serializer.Deserialize(fs);
+            }
+
+        }
+
+        public void CenaDPH(List<Vikend> vikend)
+        {
+            foreach (var item in autobazar.Auto)
+            {
+                if (item.DatumStruct.DayOfWeek == DayOfWeek.Saturday || item.DatumStruct.DayOfWeek == DayOfWeek.Sunday)
+                {
+                vikend.Add(new Vikend(item.Model, item.Cena, item.Cena + (item.Cena * item.DPH / 100)));
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Opravdu si přejete ukončit program?", "Konec",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Error) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+        private void prodejVikend_CopyingRowClipboardContent(object sender, DataGridRowClipboardEventArgs e)
+        {
+            var currentCell = e.ClipboardRowContent[prodejVikend.CurrentCell.Column.DisplayIndex];
+            e.ClipboardRowContent.Clear();
+            e.ClipboardRowContent.Add(currentCell);
+        }
+    }
+}
